de7da323e8ae009507c17a100824d2766085a66e
xizizzz <y960303802@163.com>
2022-03-21 01:17:25 +0800

    [add] 03_21

---
+++ src/leetcode每日一题/_03_21/_653.两数之和IV-输入BST.java
@@ -0,0 +1,38 @@
+package leetcode每日一题._03_21;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    List<Integer> list=new ArrayList<>();
+    public boolean findTarget(TreeNode root, int k) {
+        dfs(root);
+        Set<Integer> set=new HashSet<>();
+        for(int v:list){
+            if(set.contains(k-v)) return true;
+            set.add(v);
+        }
+        return false;
+    }
+    public void dfs(TreeNode root){
+        if(root==null) return;
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
+}
