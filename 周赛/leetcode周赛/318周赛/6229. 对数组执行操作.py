c8d3407795f70e2a1e6ae4500b0aa83490f01545
yinpeng <y960303802@163.com>
2022-11-06 15:06:54 +0800

    [add] 318周赛 11月6号

--- 周赛/leetcode周赛/318周赛/6229. 对数组执行操作.py
+++ 周赛/leetcode周赛/318周赛/6229. 对数组执行操作.py
@@ -1,6 +1,20 @@
-from typing import List, Tuple, Optional
-from collections import defaultdict, Counter
-from sortedcontainers import SortedList
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from typing import List, Tuple, Optional, Dict, Set
+from collections import defaultdict, Counter, deque, OrderedDict, namedtuple
+from sortedcontainers import SortedList, SortedDict, SortedSet
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache
+
+'''
+gcd(), ord(), chr(), lower(), upper() 最大公约数/ASCII字符数值/数值ASCII字符/小写/大写
+startswith(s), endswith(s), find(), index(), count(s)  字符串是否以s开始的/字符串是否以s结尾的/查找返回的是索引/获取索引
+isalpha(), isdigit(), space(),join()  判断是否全为字符/判断是否全为数字/判断是否为空格/拼接
+eval() 字符串转换成列表、元组或者字典/
+uniform(x, y), pow(x, y)# 随机生成下一个实数，它在[x,y]范围内/ x**y 运算后的值。
+'''
 
 # -*- coding: utf-8 -*-
 # @Author  : hakusai
