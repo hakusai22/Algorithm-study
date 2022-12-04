5d22d35eb54ef1319e1d904a0ce310fba5feaebb
yinpeng <y960303802@163.com>
2022-12-04 18:07:00 +0800

    [add]  并查集

--- py3数据结构/并查集/lc_128.最长连续序列.py
+++ py3数据结构/并查集/lc_128.最长连续序列.py
@@ -64,28 +64,49 @@ class ListNode:
 
 # -*- coding: utf-8 -*-
 # @Author  : zero
-# @Time    : 2022/12/04 12:12
+# @Time    : 2022/12/04 17:25
+"""
+一个未排序数组，要找出数字连续最长序列，且时间复杂度为n，因此只能进行一次遍历且不能进行排序，考虑使用并查集
+"""
 
 class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
+    """
+    self.parent = {100: 100, 4: 4, 200: 200, 1: 1, 3: 3, 2: 2}，自己的根节点就是自己。
+    self.count是一个字典，形如 根节点:元素个数，表示根节点后面连接子节点的个数。
+    如 1:3 表示1为根节点，后面还连着两个连续的元素，即1→2→3，共3个元素。
+    """
+    def __init__(self, nums):
+        self.parent = {num: num for num in nums}
+        self.cnt = defaultdict(lambda: 1)
 
     def find(self, x):
         while x != self.parent[x]:
             x = self.parent[x]
         return x
 
-    def merge(self, a, b):
-        root1, root2 = self.find(a), self.find(b)
+    def union(self, x, y):
+        if y not in self.parent:
+            return 1
+        # 寻找x, y的根节点，记为root1, root2
+        root1, root2 = self.find(x), self.find(y)
+
+        # 如果root1 == root2，直接返回self.cnt[root1]或者self.cnt[root2]
+        # 表示当前的数与之前已经出现的一些数字能构成连续序列
+        if root1 == root2:
+            return self.cnt[root1]
+
+        # 将root2的根节点改为root1，如下面步骤5中的图所示，将两棵子树合并
         self.parent[root2] = root1
+        # 两棵子树合并时，现在的树的元素个数就是这两棵树之和
+        self.cnt[root1] += self.cnt[root2]
+        return self.cnt[root1]
 
 class Solution:
-    def minScore(self, n: int, roads: List[List[int]]) -> int:
-        union = UnionFind(n + 1)
-        for x, y, v in roads:
-            union.merge(x, y)
-        ans = inf
-        for x, y, v in roads:
-            if union.find(x) == union.find(1):
-                ans = min(ans, v)
-        return ans
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        union = UnionFind(nums)
+        res = 1
+        for num in nums:
+            res = max(res, union.union(num, num + 1))
+        return res
