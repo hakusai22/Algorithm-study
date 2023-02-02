c8863165d4e688eca5244fbdde50a7ffcb2cd3a3
yinpeng <y960303802@163.com>
2023-02-02 16:09:56 +0800

    [add] DP 2023/02/02

--- py3动态规划/背包问题/AcWing 4. 多重背包问题.py
+++ py3动态规划/背包问题/AcWing 4. 多重背包问题.py
@@ -16,6 +16,21 @@ INFMAX = float('inf')
 # @Author  : wheat
 # @Time    : 2023/01/31 15:29
 
+
+"""
+    有 N 种物品和一个容量是 V 的背包。
+    第 i 种物品最多有 si件，每件体积是 vi，价值是 wi。
+    求解将哪些物品装入背包，可使物品体积总和不超过背包容量，且价值总和最大。
+    输出最大价值。
+    数据范围
+        0<N,V≤100
+        0<vi,wi,si≤100
+    一、状态表示：f[i][j]
+        1. 集合：从前i个物品中选,且总体积不超过j的所有方案的集合.
+        2. 属性：最大值
+        f[i][j] = max(f[i][j], f[i - 1][j - k * v[i]] + k * w[i]);
+"""
+
 if __name__ == '__main__':
     N = 110
     v = [0] * N
@@ -27,8 +42,11 @@ if __name__ == '__main__':
     for i in range(1, n + 1):
         v[i], w[i], s[i] = map(int, input().split())
 
+    # 枚举背包
     for i in range(1, n + 1):
+        # 枚举体积
         for j in range(m + 1):
+            # 枚举个数
             k = 0
             f[i][j] = f[i - 1][j]
             while 1:
--- py3动态规划/背包问题/AcWing 9. 分组背包问题.py
+++ py3动态规划/背包问题/AcWing 9. 分组背包问题.py
@@ -22,25 +22,34 @@ INFMAX = float('inf')
     每件物品的体积是 vij，价值是 wij，其中 i 是组号，j是组内编号。
     求解将哪些物品装入背包，可使物品总体积不超过背包容量，且总价值最大。
     输出最大价值。
+    
+    一、状态表示：f[i][j]
+        1. 集合：从前i组物品中选,且总体积不超过j的所有方案的集合.
+        2. 属性：最大值
+        f[i][j] = max(f[i][j], f[i - 1][j - v[i][k]] + w[i][k]);
 """
 
-N = 110
-v = [[0] * N for _ in range(N)]
-w = [[0] * N for _ in range(N)]
-s = [[0] * N for _ in range(N)]
-f = [0] * N
+if __name__ == '__main__':
+    N = 110
+    v = [[0] * N for _ in range(N)]
+    w = [[0] * N for _ in range(N)]
+    s = [[0] * N for _ in range(N)]
+    f = [0] * N
 
-n, m = map(int, input().split())
+    n, m = map(int, input().split())
 
-for i in range(0, n):
-    s[i] = int(input())
-    for j in range(0, s[i]):
-        v[i][j], w[i][j] = map(int, input().split())
+    for i in range(0, n):
+        s[i] = int(input())
+        for j in range(0, s[i]):
+            v[i][j], w[i][j] = map(int, input().split())
 
-for i in range(0, n):
-    for j in range(m, -1, -1):
-        for k in range(0, s[i]):
-            if v[i][k] <= j:
-                f[j] = max(f[j], f[j - v[i][k]] + w[i][k])
+    # 枚举物品组
+    for i in range(0, n):
+        # 枚举体积 逆序(同一组内的物品最多只能选一个)
+        for j in range(m, -1, -1):
+            # 枚举每个组里面的物品
+            for k in range(0, s[i]):
+                if v[i][k] <= j:
+                    f[j] = max(f[j], f[j - v[i][k]] + w[i][k])
 
-print(f[m])
+    print(f[m])
