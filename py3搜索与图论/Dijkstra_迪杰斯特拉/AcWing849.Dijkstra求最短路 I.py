4b6560f091ad77976e6cc3f2a3ad6fce3359f8de
yinpeng <y960303802@163.com>
2023-02-06 16:48:37 +0800

    [add] 搜索和图论

--- py3搜索与图论/Dijkstra_迪杰斯特拉/AcWing849.Dijkstra求最短路 I.py
+++ py3搜索与图论/Dijkstra_迪杰斯特拉/AcWing849.Dijkstra求最短路 I.py
@@ -6,68 +6,34 @@ from collections import defaultdict, Counter, deque, OrderedDict, namedtuple
 from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby
 from queue import PriorityQueue, Queue, LifoQueue
 from functools import lru_cache, reduce
-from copy import deepcopy
-from io import BytesIO, IOBase
-import random
-import sys
-import os
 
 MOD = int(1e9 + 7)
 INF = int(1e20)
-'''
-gcd(), ord(), chr(), lower(), upper() 最大公约数/ASCII字符数值/数值ASCII字符/小写/大写
-startswith(s), endswith(s), find(), index(), count(s)  字符串是否以s开始的/字符串是否以s结尾的/查找返回的是索引/获取索引
-isalpha(), isdigit(), space(),join()  判断是否全为字符/判断是否全为数字/判断是否为空格/拼接
-eval() 字符串转换成列表、元组或者字典/
-uniform(x, y), pow(x, y)# 随机生成下一个实数，它在[x,y]范围内/ x**y 运算后的值。
-字典推倒式 {key: len(key) for key in list}
-列表推倒式 [i for i in range(100) if i % 3 == 0] 可切片,可索引,可重复
-集合推倒式 {i ** 2 for i in (1, 2, 3)}  不可索引,不可切片,不可重复元素
-'''
 
-def I():
-    return input()
+"""
+    给定一个 n个点 m条边的有向图，图中可能存在重边和自环，所有边权均为正值。
+    请你求出 1号点到 n号点的最短距离，如果无法从 1号点走到 n号点，则输出 −1
+    输出一个整数，表示 1号点到 n 号点的最短距离。
+    数据范围
+        1≤n≤500
+        1≤m≤105
+        图中涉及边长均不超过10000。
+        
+    Dijkstra.md 的整体思路比较清晰
+        即进行n（n为n的个数）次迭代去确定每个点到起点的最小值 最后输出的终点的即为我们要找的最短路的距离
+    
+    朴素 Dijkstra。
+        设 dist[1] = 0 和 dist[i] = INF。
+        设集合 S为当前已确定最短路径的点。找出不在 S中且 dist 最小的点 t
+        用 t更新所有点的距离。
+    时间复杂度 O(n^2)
+"""
 
-def II():
-    return int(input())
 
-def FI():
-    return float(input())
-
-def MII():
-    return map(int, input().split())
-
-def MFI():
-    return map(float, input().split())
-
-def LI():
-    return list(input().split())
-
-def LMII():
-    return list(map(int, input().split()))
-
-def LMFI():
-    return list(map(float, input().split()))
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-# -*- coding: utf-8 -*-
-# @Author  : zero
-# @Time    : 2022/11/28 22:32
 if __name__ == '__main__':
     N = 510
     # 因为题目的边远大于点，因此是稠密图，稠密图用邻接矩阵去存。
+    # 用于存储每个点到起点的最短距离
     d = [[0x3f3f3f3f] * N for _ in range(N)]
     # dist为每个点到起点的距离
     dist = [0x3f3f3f3f] * N
@@ -79,13 +45,14 @@ if __name__ == '__main__':
         # 迭代 n - 1 次，因为每迭代一次，一个点的最短距离就确定了，并把它加入到确定的集合当中。
         # 前 n - 1 个点已经确定，那么最后一个点自然也就是最短距离了，所以可以把 n 改成 n - 1
         for i in range(n - 1):
-            # 随便定义一个点，主要是为了后面比较，然后找到最小距离的点
+            # 将t设置为-1 因为Dijkstra算法适用于不存在负权边的图
             t = -1
             # 循环每个点，找到最短距离的点，并把它赋值给 t
             for j in range(1, n + 1):
+                # 该步骤即寻找还未确定最短路的点中路径最短的点
                 if not st[j] and dist[j] < dist[t]:
                     t = j
-            # 标记该点距离确定
+            # 同时该点的最短路径也已经确定我们将该点标记
             st[t] = True
             # 根据该点更新其他所有点的距离
             for j in range(1, n + 1):
