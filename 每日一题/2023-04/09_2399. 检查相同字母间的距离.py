47f142f915105dd0cc147102ad515fdff4ba830c
yinpeng <y960303802@163.com>
2023-04-09 09:47:52 +0800

    [add] 04-09

---
+++ 每日一题/2023-04/09_2399. 检查相同字母间的距离.py
@@ -0,0 +1,44 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache
+import sys
+from typing import List
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')
+INFMAX = float('inf')
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def end(r=-1):
+    print(r)
+    exit()
+
+# -*- coding: utf-8 -*-
+# @Author  : wink
+# @Time    : 2023/04/09 09:07
+
+class Solution:
+    def checkDistances(self, s: str, distance: List[int]) -> bool:
+        d = defaultdict(int)
+        for i, c in enumerate(s, 1):
+            if d[c] and i - d[c] - 1 != distance[ord(c) - ord('a')]:
+                return False
+            d[c] = i
+        return True
