54bf12b4ccc462e000f5bc67f19860aa80119d60
yinpeng <y960303802@163.com>
2023-04-16 22:48:26 +0800

    04-16

---
+++ C++/cc.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "hello world11" << endl;
+    return 0;
+}
---
+++ Goland/Go_Advanced/closure/demo4.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+/*
+   -*- coding: utf-8 -*-
+   @Author  : wheat
+   @Time    : 2023/04/16 20:17
+*/
+
+func main() {
+	fmt.Println("hello")
+}
