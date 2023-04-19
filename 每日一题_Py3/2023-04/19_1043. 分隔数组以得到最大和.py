00369d0c2779deb0583466f5da39f9758f82937a
yinpeng <y960303802@163.com>
2023-04-19 22:20:10 +0800

    19

---
+++ 每日一题_Py3/2023-04/19_1043. 分隔数组以得到最大和.py
@@ -0,0 +1,17 @@
+'''
+Author: hakusai
+Date: 2023-04-19 22:12:59
+LastEditTime: 2023-04-19 22:19:40
+FilePath: /Algorithm-study/每日一题_Py3/2023-04/19_1043. 分隔数组以得到最大和.py
+'''
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+
+        n=len(arr)
+        f=[0]*(n+1)
+        for i in range(1,n+1):
+            mx=0
+            for j in range(i,max(0,i-k),-1):
+                mx=max(mx,arr[j-1])
+                f[i]=max(f[i],f[j-1]+mx*(i-j+1))
+        return f[n]
