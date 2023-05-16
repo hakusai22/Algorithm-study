ff8cc1a7425e1b431f47dde413fde204034b8095
yinpeng <y960303802@163.com>
2023-05-16 10:15:26 +0800

    lc

--- LeetCode每日一题/2023-05/16_1335. 工作计划的最低难度.py
+++ LeetCode每日一题/2023-05/16_1335. 工作计划的最低难度.py
@@ -1,10 +1,10 @@
 '''
 Author: hakusai
 Date: 2023-05-16 08:59:13
-LastEditTime: 2023-05-16 08:59:54
+LastEditTime: 2023-05-16 09:56:04
 '''
 from collections import Counter, defaultdict, deque
-from functools import lru_cache, reduce, cmp_to_key
+from functools import cache, lru_cache, reduce, cmp_to_key
 from itertools import accumulate, combinations, permutations, product
 from heapq import nsmallest, nlargest, heapify, heappop, heappush
 from bisect import bisect_left, bisect_right
@@ -15,3 +15,17 @@ from typing import List
 
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+        if n < d:
+            return -1
+
+        lru_cache(maxsize=None)
+        def dfs(i, j):
+            if i == 0:
+                return max(jobDifficulty[:j+1])
+            res, mx = inf, 0
+            for k in range(j, i-1, -1):
+                mx = max(mx, jobDifficulty[k])
+                res = min(res, dfs(i-1, k-1)+mx)
+            return res
+        return dfs(d-1, n-1)
